b569e6a4b79a79a223fa1379e578e03d490dc285
Rishabh Yadav <yrishabh24@gmail.com>
2015-06-22 20:13:57 +0530

    refactoring and bug fix

--- keyserver.rb
+++ keyserver.rb
@@ -2,7 +2,7 @@ require 'socket'
 require 'securerandom'
 require 'sinatra'
 require 'redis'
-
+require 'time'
 
 =begin
   Features
@@ -40,7 +40,7 @@ get '/generate' do
 get '/get' do
      key = KeyServer.get(redis)
      if key.nil?
-       not_found
+       "404 Not Found"
      else
        key
      end
@@ -61,60 +61,86 @@ get '/keep_alive/:key' do
     KeyServer.keep_alive(redis,params['key'])
 end
 
-#404
+
 not_found do
-  '404.Not found'
+  'Bad Request'
 end
 
 
 class KeyServer
+  
+  
 	def self.generate(redis)
-	 rand_key = SecureRandom.hex(20)
-         redis.sadd('UNBLOCKED',rand_key)
-         redis.setex(rand_key,300,Time.now)
-         if rand_key.nil?
-            return "Key can't be generated"
-         else
-            return "Key generated successfully"
-         end
-        end
-      
-       #Returns nil if 404
-       def self.get(redis)
-          key = redis.spop('UNBLOCKED')
-          if not key.nil?
-                redis.set(key,Time.now) 
-                #Move to unblocked after 60 secs
-          end
-          return key
-       end
+    #Generate a random key
+	  rand_key = SecureRandom.hex(20)
+    #Add key to set of free keys
+    redis.sadd('UNBLOCKED',rand_key)
+    #Set value and expiry time
+    redis.setex(rand_key,300,Time.now)
     
-       def self.unblock(redis,key)
-           if redis.exists(key)
-                if redis.sadd('UNBLOCKED',key)
-                   return "Key unblocked successfully."
-                else
-                   return "Key can't be unblocked"
-               end
-           else
-                   return "No such key exists."
-          end
-       end
+    if rand_key.nil?
+        return "Key can't be generated"
+    else
+        return "Key generated successfully"
+    end
+  end
+      
+  #Returns nil if 404
+  def self.get(redis)
+      key = redis.spop('UNBLOCKED')
+      if not key.nil?
+        #Update the time stamp
+        redis.set(key,Time.now)
+        #list to handle the 60 secs timeout of keys
+        redis.lpush('BLOCKED',key)
+        return key
+      else 
+        key = redis.rpop('BLOCKED')
+        if not key.nil?
+            time_stamp = redis.get(key)
+            if (Time.parse(time_stamp)-Time.now).abs >=60
+              redis.lpush('BLOCKED',key)
+              redis.set(key,Time.now)
+              return key
+            else
+              redis.rpush('BLOCKED',key)
+              return nil
+            end
+        else
+            return nil
+        end            
+      end
+      return nil
+  end
+    
+   def self.unblock(redis,key)
+      if redis.exists(key)     
+            if redis.sadd('UNBLOCKED',key)
+                redis.lrem('BLOCKED',1,key) 
+                return "Key unblocked successfully."
+            else
+                return "Key already unblocked."
+            end
+      else
+          return "No such key exists."
+      end
+    end
 
-       def self.delete(redis,key)
-           if redis.exists(key)
-                if redis.del(key)==1
-                    if redis.sismember('UNBLOCKED',key)==1
+    def self.delete(redis,key)
+       if redis.exists(key)
+            if redis.del(key)==1
+                  if redis.sismember('UNBLOCKED',key)==1
                         redis.srem('UNBLOCKED',key)
-                    end
-                   return "Key deleted successfully."
-                else
-                   return "key can't be deleted."
-                end
-           else 
-                   return "No such key exists."
-          end
-       end
+                  end
+                  redis.lrem('BLOCKED',1,key)
+                  return "Key deleted successfully."
+            else
+                  return "key can't be deleted."
+            end
+        else 
+            return "No such key exists."
+        end
+      end
 
        def self.keep_alive(redis,key)
            if redis.exists(key)
@@ -123,9 +149,5 @@ class KeyServer
            else
                 return "No such key exists."
            end
-       end
-   
-       def self.error
-           '404.Not found'
        end
 end
